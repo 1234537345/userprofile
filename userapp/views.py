5f7ce9be27182a06fbe0b0d20ebc11369770f6da
1234537345 <veenastarwing98@gmail.com>
2024-08-08 17:55:14 +0530

    Update views.py

--- userapp/views.py
+++ userapp/views.py
@@ -1,70 +1,92 @@
-from django.contrib.auth import models
-from django.core.paginator import Paginator, EmptyPage,PageNotAnInteger
 from django.shortcuts import render, redirect
-# myapp/views.py
-from django.contrib import messages
-from .models import userprofile
-from django.contrib.auth.models import User
+from .models import resumeprofile
 
 
-from django.shortcuts import render, redirect
-from django.contrib.auth.models import User
-from django.contrib import messages
-
-def createuserProfile(request):
-
+def createprofile(request):
     if request.method == 'POST':
         fullname = request.POST.get('fullname')
-        DOB = request.POST.get('DOB')
-        bio = request.POST.get('bio')
-        skills = request.POST.get('skills')
-        email = request.POST.get('email')
         phone = request.POST.get('phone')
-        p = userprofile(fullname=fullname, DOB=DOB,bio=bio,skills=skills,email=email,phone=phone)
-        p.save()
+        address = request.POST.get('address')
+        email = request.POST.get('email')
+        universityCollege = request.POST.get('universityCollege')
+        DegreeCertification = request.POST.get('DegreeCertification')
+        graduation = request.POST.get('graduation')
+        skills = request.POST.get('skills')
+        projecttitle = request.POST.get('projecttitle')
+        projectdescription = request.POST.get('projectdescription')
+        coursetitle = request.POST.get('coursetitle')
+        prjectimg = request.POST.get('prjectimg')
+        prjectlink = request.POST.get('prjectlink')
+        aboutme = request.POST.get('aboutme')
+        objective = request.POST.get('objective')
+        p = resumeprofile(
+            fullname=fullname,
+            phone=phone,
+            address=address,
+            email=email,
+            universityCollege=universityCollege,
+            DegreeCertification=DegreeCertification,
+            graduation=graduation,
+            skills=skills,
+            projecttitle=projecttitle,
+            projectdescription=projectdescription,
+            coursetitle=coursetitle,
+            prjectimg=prjectimg,
+            prjectlink=prjectlink,
+            aboutme=aboutme,
+            objective=objective
 
-    return render(request, 'userprofile.html')
+        )
+        p.save()
+    return render(request, 'profile.html')
 
 
 def listdetails(request):
-    values = userprofile.objects.all()
-    paginator = Paginator(values, 3)
-    page_number = request.GET.get('page')
+    values = resumeprofile.objects.last()
 
+    return render(request, 'resume.html', {'values': values})
 
-    try:
-        page = paginator.page(page_number)
-    except PageNotAnInteger:
-        page = paginator.page(1)
-    except EmptyPage:
-        page = paginator.page(paginator.num_pages)
-
-    return render(request, 'printvalues.html', {'values': values, 'page': page})
 
 def updatedetails(request, p_id):
-    p=userprofile.objects.get(id=p_id)
-    if request.method=='POST':
+    p = resumeprofile.objects.get(id=p_id)
+    if request.method == 'POST':
         fullname = request.POST.get('fullname')
-        DOB = request.POST.get('DOB')
-        bio = request.POST.get('bio')
-        skills = request.POST.get('skills')
-        email = request.POST.get('email')
         phone = request.POST.get('phone')
-        p.fullname=fullname
-        p.DOB=DOB
-        p.bio = bio
-        p.skills = skills
-        p.email = email
+        address = request.POST.get('address')
+        email = request.POST.get('email')
+        universityCollege = request.POST.get('universityCollege')
+        DegreeCertification = request.POST.get('DegreeCertification')
+        graduation = request.POST.get('graduation')
+        skills = request.POST.get('skills')
+        projecttitle = request.POST.get('projecttitle')
+        projectdescription = request.POST.get('projectdescription')
+        coursetitle = request.POST.get('coursetitle')
+        prjectimg = request.POST.get('prjectimg')
+        prjectlink = request.POST.get('prjectlink')
+        objective = request.POST.get('objective')
+        aboutme = request.POST.get('aboutme')
+
+        p.fullname = fullname
         p.phone = phone
+        p.address = address
+        p.email = email
+        p.universityCollege = universityCollege
+        p.DegreeCertification = DegreeCertification
+        p.graduation = graduation
+        p.skills = skills
+        p.projecttitle = projecttitle
+        p.projectdescription = projectdescription
+        p.coursetitle = coursetitle
+        p.prjectimg = prjectimg
+        p.prjectlink = prjectlink
+        p.objective = objective
+        p.aboutme = aboutme
         p.save()
         return redirect('details')
-    return render(request, 'updatevalues.html', {'p':p})
+    return render(request, 'updatedetails.html', {'p': p})
 
-def deletedetails(request,p_id):
-    p = userprofile.objects.get(id=p_id)
+
+def deletedetails(request, p_id):
+    p = resumeprofile.objects.get(id=p_id)
     p.delete()
     return redirect('details')
-    return render(request, 'printvalues.html', {'p': p})
-
-def index(request):
-    return render(request, 'base.html')
